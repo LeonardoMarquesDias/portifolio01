b02e69bfbf4922fdbf5668659a3eac55d27e0920
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-06-17 01:36:56 +0100

    xmenu done

--- xmenu.js
+++ xmenu.js
@@ -3,10 +3,13 @@ let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
 
-menuToggle.addEventListener("click", () => {
+ menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial"
 
     menuSection.classList.toggle("on", show)
     show = !show;
+
+    var element = document.getElementById("nav");
+    element.classList.toggle("nav_style");
 })
